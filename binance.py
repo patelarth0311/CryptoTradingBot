3551c182afcfc2d4e1bda0853e8ffe9ff9be2ba6
patelarth0311 <patelarth0311@gmail.com>
2022-08-13 22:58:54 -0400

    Introduce meaningful methods and organize code

--- binance.py
+++ binance.py
@@ -1,3 +1,4 @@
+from ast import ExceptHandler
 import urllib.parse
 import hashlib
 import hmac
@@ -13,48 +14,64 @@ import matplotlib.pyplot as plt
 
 ### WORK IN PROGRESS
 
-binanceUS = 'binanceus'
-exchange_class = getattr(ccxt, binanceUS)
-exchange = exchange_class({
+
+def engage_binance_client():
+    binanceUS = 'binanceus'
+    exchange_class = getattr(ccxt, binanceUS)
+    exchange = exchange_class({
     'apiKey': 'qZd0TVwy6nCgxWvRiYoEoOEG9N2lEthcTv7Zbc8xjegLYIg94pDdGcKfoYXibYge',
     'secret': 'uDtR7qHQNooW2OcdITnhj6DWGN78ErLoOYRUP2Fc2YqhG7UuMlnbJ7XOdhjg8jQJ',
-})
+    })
+    return exchange
 
+client = engage_binance_client()
 
-
-
-candles = exchange.fetch_index_ohlcv('BTCUSDT', timeframe='4h')
+def create_ohlcv_df():
+    
+    candles = client.fetch_index_ohlcv('BTCUSDT', timeframe='4h')
 # https://github.com/ccxt/ccxt/blob/19e13b5867311107ad119ad1f35d90fd2ffc6cc3/python/ccxt/base/exchange.py
-for i in candles:
-  del i[-1]
+    for i in candles:
+        del i[-1]
 
-df = pd.DataFrame (candles, columns = ['date', 'open','high','low','close'])
-df.set_index('date', inplace=True)
-df.index = pd.to_datetime(df.index, unit='ms')
-
+    df = pd.DataFrame (candles, columns = ['date', 'open','high','low','close'])
+    df.set_index('date', inplace=True)
+    df.index = pd.to_datetime(df.index, unit='ms')
+    return df
 
 #MACD
-df['ema_12'] = df['close'].ewm(span=12,adjust=False).mean()
-df['ema_26'] = df['close'].ewm(span=26,adjust=False).mean()
-df['MACD'] = df['ema_12'] - df['ema_26']
-df['signal'] = df['MACD'].ewm(span=9,adjust=False).mean()
-df['histogram'] = df['MACD'] - df['signal']
+def macd(df):
+    macd_df = pd.DataFrame()
+    macd_df['ema_12'] = df['close'].ewm(span=12,adjust=False).mean()
+    macd_df['ema_26'] = df['close'].ewm(span=26,adjust=False).mean()
+    macd_df['macd'] = macd_df['ema_12'] - macd_df['ema_26']
+    macd_df['signal'] = macd_df['macd'].ewm(span=9,adjust=False).mean()
+    macd_df['histogram'] = macd_df['macd'] - macd_df['signal']
+    return macd_df
 
 #RSI
-close_delta = df['close'].diff()
-positive_delta = close_delta.clip(lower=0)
-negative_delta = close_delta.clip(upper=0) * -1
-ema_up = positive_delta.ewm(com=13, adjust=True, min_periods = 14).mean()
-ema_down = negative_delta.ewm(com=13, adjust=True, min_periods = 14).mean()
-rs = ema_up/ema_down
-rsi = 100 - (100/(1 + rs))
-df['rsi'] =  rsi 
+def rsi(df):
+    rsi_df = pd.DataFrame()
+    close_delta = df['close'].diff()
+    positive_delta = close_delta.clip(lower=0)
+    negative_delta = close_delta.clip(upper=0) * -1
+    ema_up = positive_delta.ewm(com=13, adjust=True, min_periods = 14).mean()
+    ema_down = negative_delta.ewm(com=13, adjust=True, min_periods = 14).mean()
+    rs = ema_up/ema_down
+    rsi = 100 - (100/(1 + rs))
+    rsi_df['rsi'] =  rsi 
+    return rsi_df
 
+#EMA200
+def ema_200(df):
+    ema_200_df = pd.DataFrame()
+    ema_200_df['ema_200'] = df['close'].ewm(span=200,adjust=False).mean()
+    return ema_200_df
 
-# 200 EMA to check if crypto is in a up/down trend
-df['ema_200'] = df['close'].ewm(span=200,adjust=False).mean()
+def aggregate_df():
+    df = create_ohlcv_df()
+    df = df.join(other = [rsi(df),macd(df),ema_200(df)], how = "inner")
 
-
+    return df
 
 #df['MACD'].plot()
 #df['histogram'].plot()
@@ -65,10 +82,19 @@ df['ema_200'] = df['close'].ewm(span=200,adjust=False).mean()
 #plt.show()
 
 
+# Take a long when EMA fast is above EMA slow and both are below the zero line
+# and when the candle closes above 200 EMA 
 
-# Take a long when EMA fast is above EMA slow below the zero line
-# and when the candle is above 200 EMA 
-print(df['MACD'].tail(1))
-print(df['signal'].tail(1))
-print(df['close'].tail(1))
-print(df['ema_200'].tail(1))
+def check_buying_conditions():
+    latest_timeframe_entry_df = aggregate_df().tail(1)
+
+    below_zero = latest_timeframe_entry_df.macd[0] < 0 and latest_timeframe_entry_df.signal[0]  < 0
+    macd_greater = latest_timeframe_entry_df.macd[0] > latest_timeframe_entry_df.signal[0] 
+    closed_above_ema_200 = latest_timeframe_entry_df.close[0] > latest_timeframe_entry_df.ema_200[0]
+    rsi_oversold = latest_timeframe_entry_df.rsi[0] < 30
+
+
+    if below_zero and macd_greater and closed_above_ema_200 and rsi_oversold:
+        print("BUY")
+
+    ## else look at the 2 hr timeframe maybe
