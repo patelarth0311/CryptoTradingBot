e11f1a4963b2feca9adf30477b73fd98ec35b7f6
patelarth0311 <patelarth0311@gmail.com>
2022-12-03 12:05:03 -0500

    More styling

--- binance.py
+++ binance.py
@@ -1,3 +1,8 @@
+"""Crypto Trading Bot- engages with the BinanceUS API to
+execute orders based on favorable market conditions-
+that being: For buying (1) MACD and Signal Line is less than zero,
+(2) MACD is greater than Signal Line, (3) currency closed above the ema 200;
+and for selling, the opposite is expected to occurr."""
 import os
 import ccxt
 import pandas as pd
@@ -9,10 +14,10 @@ from dotenv import load_dotenv
 
 
 def engage_binance_client() -> ccxt.binanceus.binanceus:
-    load_dotenv()
     """Establish connection with BinanceUSD"""
-    binance_US = 'binanceus'
-    exchange_class = getattr(ccxt, binance_US)
+    load_dotenv()
+    binance_us = 'binanceus'
+    exchange_class = getattr(ccxt, binance_us)
     exchange = exchange_class({
     'apiKey': os.getenv('API_KEY'),
     'secret': os.getenv('API_SECRET')
@@ -27,27 +32,27 @@ def create_ohlcv_df(timeframe: str) -> pd.DataFrame:
 # https://github.com/ccxt/ccxt/blob/19e13b5867311107ad119ad1f35d90fd2ffc6cc3/python/ccxt/base/exchange.py
     for i in candles:
         del i[-1]
-    df = pd.DataFrame (candles, columns = ['date', 'open','high','low','close'])
-    df.set_index('date', inplace=True)
-    df.index = pd.to_datetime(df.index, unit='ms')
-    return df
+    d_f = pd.DataFrame (candles, columns = ['date', 'open','high','low','close'])
+    d_f.set_index('date', inplace=True)
+    d_f.index = pd.to_datetime(d_f.index, unit='ms')
+    return d_f
 
 #MACD
-def macd(df: pd.DataFrame) -> pd.DataFrame:
+def macd(d_f: pd.DataFrame) -> pd.DataFrame:
     """Returns dataframe representative of the macd"""
     macd_df = pd.DataFrame()
-    macd_df['ema_12'] = df['close'].ewm(span=12,adjust=False).mean()
-    macd_df['ema_26'] = df['close'].ewm(span=26,adjust=False).mean()
+    macd_df['ema_12'] = d_f['close'].ewm(span=12,adjust=False).mean()
+    macd_df['ema_26'] = d_f['close'].ewm(span=26,adjust=False).mean()
     macd_df['macd'] = macd_df['ema_12'] - macd_df['ema_26']
     macd_df['signal'] = macd_df['macd'].ewm(span=9,adjust=False).mean()
     macd_df['histogram'] = macd_df['macd'] - macd_df['signal']
     return macd_df
 
 #RSI
-def rsi(df: pd.DataFrame) -> pd.DataFrame:
+def rsi(d_f: pd.DataFrame) -> pd.DataFrame:
     """Returns dataframe representative of a rsi"""
     rsi_df = pd.DataFrame()
-    close_delta = df['close'].diff()
+    close_delta = d_f['close'].diff()
     positive_delta = close_delta.clip(lower=0)
     negative_delta = close_delta.clip(upper=0) * -1
     ema_up = positive_delta.ewm(com=13, adjust=True, min_periods = 14).mean()
@@ -58,32 +63,32 @@ def rsi(df: pd.DataFrame) -> pd.DataFrame:
     return rsi_df
 
 #EMA200
-def ema_200(df: pd.DataFrame) -> pd.DataFrame:
+def ema_200(d_f: pd.DataFrame) -> pd.DataFrame:
     """Returns dataframe representative of an ema_200"""
     ema_200_df = pd.DataFrame()
-    ema_200_df['ema_200'] = df['close'].ewm(span=200,adjust=False).mean()
+    ema_200_df['ema_200'] = d_f['close'].ewm(span=200,adjust=False).mean()
     return ema_200_df
 
-#combining df
+#combining d_f
 def aggregate_df(timeframe: str) -> pd.DataFrame:
     """Aggregates dataframes returned by rsi, macd, and emma_200 by an inner join"""
-    df = create_ohlcv_df(timeframe)
-    df = df.join(other = [rsi(df),macd(df),ema_200(df)], how = "inner")
+    d_f = create_ohlcv_df(timeframe)
+    d_f = d_f.join(other = [rsi(d_f),macd(d_f),ema_200(d_f)], how = "inner")
 
-    return df
+    return d_f
 
 # Take a long when EMA fast is above EMA slow and both are below the zero line
-# and when the candle closes above 200 EMA 
+# and when the candle closes above 200 EMA
 def check_buying_conditions(timeframe: str) -> tuple([pd.DataFrame, bool]):
-    """Returns latest row from the dataframe returned 
+    """Returns latest row from the dataframe returned
     from aggregated_df and a boolean indiciative of an existence of flavorable buying conditions"""
     latest_timeframe_entry_df = aggregate_df(timeframe).tail(1)
     below_zero = latest_timeframe_entry_df.macd[0] < 0 and latest_timeframe_entry_df.signal[0]  < 0
-    macd_greater = latest_timeframe_entry_df.macd[0] > latest_timeframe_entry_df.signal[0] 
+    macd_greater = latest_timeframe_entry_df.macd[0] > latest_timeframe_entry_df.signal[0]
     closed_above_ema_200 = latest_timeframe_entry_df.close[0] > latest_timeframe_entry_df.ema_200[0]
     rsi_oversold = latest_timeframe_entry_df.rsi[0] < 30
-    return latest_timeframe_entry_df, (below_zero and macd_greater and closed_above_ema_200 and rsi_oversold)
-
+    return latest_timeframe_entry_df, (below_zero and macd_greater
+    and closed_above_ema_200 and rsi_oversold)
 
 def get_balance() -> float:
     """Checks for available USDT"""
@@ -91,7 +96,7 @@ def get_balance() -> float:
     for available in client.fetch_balance()['info']['balances']:
         if available['asset'] == 'USDT':
             balance = (available['free'])
-            break  
+            break
     return balance
 
 def take_order() -> None:
@@ -106,11 +111,12 @@ def take_order() -> None:
     }
     if  check_4hr_timeframe or check_2hr_timeframe:
         client.create_order( symbol = 'BTCUSDT',  type = 'STOP_LOSS_LIMIT',
-        side = 'buy', price = latest_timeframe_entry_df.close[0], amount = (balance * .20), params = params)
-#df['MACD'].plot()
-#df['histogram'].plot()
-#ax = df['signal'].plot()
-#df['close'].plot(ax=ax,secondary_y=True)
-#df['ema_200'].plot()
-#df['rsi'].plot()
+        side = 'buy', price = latest_timeframe_entry_df.close[0],
+        amount = (balance * .20), params = params)
+#d_f['MACD'].plot()
+#d_f['histogram'].plot()
+#ax = d_f['signal'].plot()
+#d_f['close'].plot(ax=ax,secondary_y=True)
+#d_f['ema_200'].plot()
+#d_f['rsi'].plot()
 #plt.show()
